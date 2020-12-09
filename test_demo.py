3820dd713ffbf931938feadc3f6184254a0526f4
playless <283408607@qq.com>
2020-12-09 21:18:54 +0800

    第二次修改

--- test_demo.py
+++ test_demo.py
@@ -1,6 +1,21 @@
 import pytest
+def setup_function():
+    pass
+def setup_module():
+    print("整个模块只执行一次")
 class TestDemo:
+    def setup_class(self):
+        print("整个类开始之前执行一次")
+    def teardown_class(self):
+        print("类结束之后执行")
+    def setup_method(self):
+        print("每个用例开始之前运行")
+    def teardown_method(self):
+        print("每个用例结束后执行")
     def test_one(self):
         a=5
         b=1
-        assert  a==b
+        assert  a!=b
+        print("我是第一个")
+    def test_two(self):
+        print("我是帅哥")
