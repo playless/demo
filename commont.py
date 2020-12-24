c1db22f18090e7a2f36d87af558441e278811f61
playless <283408607@qq.com>
2020-12-24 19:40:29 +0800

    commont commit

---
+++ commont.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os
+
+
+class Commont:
+    def filepath(self,dir,filename):
+        # print(os.path.dirname(__file__))
+        return os.path.join(os.path.dirname(__file__),dir,filename)
+
+# if __name__ == '__main__':
+#     com=Commont()
+#     print(com.filepath("webSelenium\pages","data.yaml"))
